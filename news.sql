2bf850f7fcd68cac6c8020b24e917d43d51e714f
Qilix <leevalenti@mail.ru>
2022-11-29 14:35:07 +0300

    changed sql

--- news.sql
+++ news.sql
@@ -1,9 +1,8 @@
 CREATE TABLE `news` (
-    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `id` mediumint(10) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `text` TEXT NOT NULL,
-    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `deleted_at` datetime DEFAULT NULL,
+    `created_at` TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
